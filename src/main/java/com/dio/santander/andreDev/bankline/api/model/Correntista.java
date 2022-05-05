c80051b9f3a738f781357f0f615d2b0c64aa1b41
Andre Santana <andrelusantana@hotmail.com>
2022-05-05 18:55:51 -0300

    Adicao anotações JPA

--- src/main/java/com/dio/santander/andreDev/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/andreDev/bankline/api/model/Correntista.java
@@ -1,38 +1,59 @@
 package com.dio.santander.andreDev.bankline.api.model;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_correntista")
 public class Correntista {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String cpf;
 	private String nome;
 	
+	@Embedded
 	private Conta conta;
-	
+
+	public Correntista() {
+
+	}
+
 	public Conta getConta() {
 		return conta;
 	}
+
 	public void setConta(Conta conta) {
 		this.conta = conta;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
-	
+
 }
