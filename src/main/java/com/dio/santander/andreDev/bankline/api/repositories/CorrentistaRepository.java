6ab14c94147afad4219ac25782959378109152b1
Andre Santana <andrelusantana@hotmail.com>
2022-05-06 09:54:21 -0300

    Ajustes classe CorrentistaRepository

--- src/main/java/com/dio/santander/andreDev/bankline/api/repositories/CorrentistaRepository.java
+++ src/main/java/com/dio/santander/andreDev/bankline/api/repositories/CorrentistaRepository.java
@@ -1,9 +1,13 @@
 package com.dio.santander.andreDev.bankline.api.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dio.santander.andreDev.bankline.api.model.Correntista;
 
 public interface CorrentistaRepository extends JpaRepository<Correntista, Integer>{
 
+	
 }
