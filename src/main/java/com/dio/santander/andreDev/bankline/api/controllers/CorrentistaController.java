10c3dbc3c636931d6d9e457273214ebe52aef628
Andre Santana <andrelusantana@hotmail.com>
2022-05-06 10:28:55 -0300

    Criação método findById classe CorrentistaController

--- src/main/java/com/dio/santander/andreDev/bankline/api/controllers/CorrentistaController.java
+++ src/main/java/com/dio/santander/andreDev/bankline/api/controllers/CorrentistaController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dio.santander.andreDev.bankline.api.Dto.CorrentistaDto;
+import com.dio.santander.andreDev.bankline.api.model.Correntista;
 import com.dio.santander.andreDev.bankline.api.services.CorrentistaService;
 
 @RestController
@@ -25,4 +27,9 @@ public class CorrentistaController {
 		
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<CorrentistaDto> findById(@PathVariable Integer id){
+		Correntista correntista = service.findById(id);
+		return ResponseEntity.ok().body(new CorrentistaDto(correntista));
+	}
 }
