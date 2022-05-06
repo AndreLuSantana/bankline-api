9e40af3ddef7da49c0bb8bbf054adde6bd6dd441
Andre Santana <andrelusantana@hotmail.com>
2022-05-06 09:54:55 -0300

    Ajustes classe CorrentistaService

--- src/main/java/com/dio/santander/andreDev/bankline/api/services/CorrentistaService.java
+++ src/main/java/com/dio/santander/andreDev/bankline/api/services/CorrentistaService.java
@@ -1,10 +1,13 @@
 package com.dio.santander.andreDev.bankline.api.services;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dio.santander.andreDev.bankline.api.Dto.CorrentistaDto;
 import com.dio.santander.andreDev.bankline.api.model.Correntista;
 import com.dio.santander.andreDev.bankline.api.repositories.CorrentistaRepository;
 
@@ -15,13 +18,11 @@ public class CorrentistaService {
 	@Autowired
 	private CorrentistaRepository repository;
 	
-	public List<Correntista> findAll() {
+	public List<CorrentistaDto> findAll() {
 
 		List<Correntista> correntistas = repository.findAll();
 		
-		return correntistas;
+		return correntistas.stream().map(x-> new CorrentistaDto(x)).collect(Collectors.toList());
 	}
-
-	
 	
 }
