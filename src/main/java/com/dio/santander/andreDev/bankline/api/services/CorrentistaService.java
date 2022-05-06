c19f8fba60598093e72ff70cae0f1e0b463d85b2
Andre Santana <andrelusantana@hotmail.com>
2022-05-06 10:29:30 -0300

    Criação método findById classe CorrentistaService

--- src/main/java/com/dio/santander/andreDev/bankline/api/services/CorrentistaService.java
+++ src/main/java/com/dio/santander/andreDev/bankline/api/services/CorrentistaService.java
@@ -1,7 +1,7 @@
 package com.dio.santander.andreDev.bankline.api.services;
 
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +10,24 @@ import org.springframework.stereotype.Service;
 import com.dio.santander.andreDev.bankline.api.Dto.CorrentistaDto;
 import com.dio.santander.andreDev.bankline.api.model.Correntista;
 import com.dio.santander.andreDev.bankline.api.repositories.CorrentistaRepository;
-
+import com.dio.santander.andreDev.bankline.api.services.exception.ObjectNotFoundException;
 
 @Service
 public class CorrentistaService {
 
 	@Autowired
 	private CorrentistaRepository repository;
-	
+
 	public List<CorrentistaDto> findAll() {
 
 		List<Correntista> correntistas = repository.findAll();
-		
-		return correntistas.stream().map(x-> new CorrentistaDto(x)).collect(Collectors.toList());
+
+		return correntistas.stream().map(x -> new CorrentistaDto(x)).collect(Collectors.toList());
 	}
-	
+
+	public Correntista findById(Integer id) {
+		Optional<Correntista> correntista = repository.findById(id);
+		return correntista.orElseThrow(() -> new ObjectNotFoundException("Correntista inexistente."));
+	}
+
 }
