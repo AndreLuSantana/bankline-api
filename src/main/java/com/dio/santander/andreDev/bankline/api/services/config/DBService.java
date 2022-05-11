b6c3b952690a12b54aefdc1901103932510c1642
Andre Santana <andrelusantana@hotmail.com>
2022-05-10 21:38:52 -0300

    Instanciação de algumas movimentações

--- src/main/java/com/dio/santander/andreDev/bankline/api/services/config/DBService.java
+++ src/main/java/com/dio/santander/andreDev/bankline/api/services/config/DBService.java
@@ -1,5 +1,6 @@
 package com.dio.santander.andreDev.bankline.api.services.config;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +8,19 @@ import org.springframework.stereotype.Service;
 
 import com.dio.santander.andreDev.bankline.api.model.Conta;
 import com.dio.santander.andreDev.bankline.api.model.Correntista;
+import com.dio.santander.andreDev.bankline.api.model.Movimentacao;
+import com.dio.santander.andreDev.bankline.api.model.MovimentacaoTipo;
 import com.dio.santander.andreDev.bankline.api.repositories.CorrentistaRepository;
+import com.dio.santander.andreDev.bankline.api.repositories.MovimentacaoRepository;
 
 @Service
 public class DBService {
 
 	@Autowired
 	private CorrentistaRepository correntistaRepository;
+	
+	@Autowired
+	private MovimentacaoRepository movimentacaoRepository;
 
 	public void instanciaDB() {
 
@@ -33,5 +40,23 @@ public class DBService {
 
 		correntistaRepository.save(correntista);
 		correntistaRepository.save(correntista1);
+		
+		/*
+		 * Movimentacao movimentacao = new Movimentacao();
+		 * movimentacao.setDescricao("Depósito");
+		 * movimentacao.setTipo(MovimentacaoTipo.RECEITA); movimentacao.setIdConta(1);
+		 * movimentacao.setValor(2500.00);
+		 */
+		
+		/*
+		 * Movimentacao movimentacao1 = new Movimentacao();
+		 * movimentacao.setDescricao("Débito - Mercado");
+		 * movimentacao.setTipo(MovimentacaoTipo.DESPESA); movimentacao.setIdConta(1);
+		 * movimentacao.setValor(1000.00);
+		 */
+		
+		
+		//movimentacaoRepository.saveAll(Arrays.asList(movimentacao));
+		
 	}
 }
